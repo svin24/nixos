713d5f978b21072f8498717f5b1172bfb5212072
svin24 <svin@dismail.de>
2024-03-04 14:56:56 +0200

    libreoffice

--- hosts/x360/configuration.nix
+++ hosts/x360/configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ../../modules/office.nix
       ../../modules/nix-ld.nix
       ../../modules/syspkg.nix
       ../../modules/java.nix
