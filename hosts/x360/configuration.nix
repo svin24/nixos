64e574dd59bb5bfede9859e3d6076da89e9c6898
svin24 <svin@dismail.de>
2024-03-05 11:07:40 +0200

    remove a bunch of comments

--- hosts/x360/configuration.nix
+++ hosts/x360/configuration.nix
@@ -23,12 +23,9 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.device="nodev";
-  networking.hostName = "nyx"; # Define your hostname.
+  networking.hostName = "nyx";
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   zramSwap.enable = true;
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
@@ -91,11 +88,7 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
     #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
 
@@ -116,9 +109,14 @@
    };
   };
 
+  environment.systemPackages = [
+    pkgs.home-manager
+  ];
+
   environment.shellAliases = {
   	vim = "nvim";
   };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -126,6 +124,7 @@
   #   enable = true;
   #   enableSSHSupport = true;
   # };
+
   security.sudo.extraRules= [
   {  users = [ "john" ];
     commands = [
@@ -134,11 +133,7 @@
       }
     ];
   }];
-  # List services that you want to enable:
-
-  environment.systemPackages = [
-    pkgs.home-manager
-  ];
+  
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
@@ -148,12 +143,6 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "unstable"; # Did you read the comment?
 
 }
